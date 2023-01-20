2f244a3b54899e8abb67fa7e41e0ba1e9a288754
junyoungNA <nanasooo@naver.com>
2023-01-20 17:33:21 +0900

    ADD event행사게시글 페이지 구현중

--- src/core/api/event.ts
+++ src/core/api/event.ts
@@ -1,2 +1,16 @@
 import sweetAlert from '../utils/sweetAlert';
 import { instance, baseURL, subURL } from '../axios/axios';
+
+export const getEvent = async () => {
+  //Feed 최신 게시물 조회
+  try {
+    // console.log(payload, 'payload');
+    //event행사 게시물조회조회
+    return await instance.get('/event');
+  } catch (error) {
+    if (error.response.request.status === 401) {
+      sweetAlert(1000, 'error', '로그인이 필요합니다!');
+    }
+    return error;
+  }
+};
