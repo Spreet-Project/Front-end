1f7155a922c4b985fc40aafd40197d030a9f707b
junyoungNA <nanasooo@naver.com>
2023-01-21 01:43:41 +0900

    배포시 영상 오류 해결중

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -182,7 +182,7 @@ const Main = (): JSX.Element => {
                         <MainVideo
                           width={'100%'}
                           height={'500px'}
-                          src={'/video/Spreetintro.mp4'}
+                          src={'videos/spreetintro.mp4'}
                         />
                       ) : (
                         <div
