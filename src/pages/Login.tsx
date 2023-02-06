456a222aca93a60f6490aecd8626f7f59c7b3858
junyoungNA <nanasooo@naver.com>
2023-02-06 17:22:09 +0900

    Fix  회원가입,마이페이지 비밀변호 변경,  비밀번호 찾기 이메일 인증 시간 기다리는 예외처리 추가, 소셜 로그인 e.preventDefault 추가 (오류 알람창 안뜨게)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -82,14 +82,16 @@ const Login = () => {
         <div className="login-social-btn">
           <button
             className="login-naver"
-            onClick={() => {
+            onClick={e => {
+              e.preventDefault();
               window.location.href =
                 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=eF_JzIGRrOBli_bLicf5&redirect_uri=https://www.spreet.co.kr/api/user/naver/callback&state=hello';
             }}
           ></button>
           <button
             className="login-kakao"
-            onClick={() => {
+            onClick={e => {
+              e.preventDefault();
               window.location.href =
                 'https://kauth.kakao.com/oauth/authorize?client_id=a2347db1ceee37de238b04db40b8bb4e&redirect_uri=https://www.spreet.co.kr/api/user/kakao/callback&response_type=code';
             }}
